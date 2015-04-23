8893c0ef0c5c2bfa3429d951f368caa2afe3b183
Sammy Lin <sammylintw@gmail.com>
2015-04-24 01:52:12 +0800

    fix spec

--- spec/lita/handlers/bingo/engineer_spec.rb
+++ spec/lita/handlers/bingo/engineer_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
 describe Lita::Handlers::Bingo::Engineer, lita_handler: true do
-  it { is_expected.to route("工程師").to(:engineer) }
-  it { is_expected.to route("找工程師").to(:engineer) }
-  it { is_expected.to route("找engineer").to(:engineer) }
-  it { is_expected.to route("可以凹他嗎").to(:engineer) }
+  it { is_expected.to route_command("工程師").to(:engineer) }
+  it { is_expected.to route_command("找工程師").to(:engineer) }
+  it { is_expected.to route_command("找engineer").to(:engineer) }
+  it { is_expected.to route_command("可以凹他嗎").to(:engineer) }
 end
