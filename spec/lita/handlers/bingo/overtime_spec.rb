25b95bc05f1b2ba98d2bbc74130d650f5409e7d0
Sammy Lin <sammylintw@gmail.com>
2015-04-24 02:21:08 +0800

    remove some spec

--- spec/lita/handlers/bingo/overtime_spec.rb
+++ spec/lita/handlers/bingo/overtime_spec.rb
@@ -2,10 +2,4 @@ require "spec_helper"
 
 describe Lita::Handlers::Bingo::Overtime, lita_handler: true do
   it { is_expected.to route_command("我要加班了").to(:overtime) }
-  describe "#cowsay" do
-    it "responds with text formatted with cowsay" do
-      send_command "我要加班了"
-      p replies.first
-    end
-  end
 end
