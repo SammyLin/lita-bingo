50f0a5827ecd065f470e9013ea09a4b6cb863ad8
Sammy Lin <sammylintw@gmail.com>
2015-04-24 10:58:55 +0800

    fix path issues

--- lib/lita/handlers/bingo/load_bingo.rb
+++ lib/lita/handlers/bingo/load_bingo.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 module LoadBingo
   def bingo(projcet, step)
-    dictionary = YAML.load_file('bingo.yml')
+    dictionary = YAML.load_file(File.expand_path('../../../../../bingo.yml', __FILE__))
     dictionary["bingo"][projcet.to_s][step.to_s].sample
   end
 end
